2df647c707ea59a2bae56291c48e041a7b217bb5
DANIEL DE MELO ANDRADE <daniel.andrade@a.ucb.br>
2025-03-07 09:28:09 -0300

    primeira atividade

---
+++ variaveisOperadores.js
@@ -0,0 +1,23 @@
+// Declaração das variáveis e operações
+let numA = 10;
+let numB = 5;
+
+// Adição
+let resultadoAdicao = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB} é ${resultadoAdicao}`);
+
+// Subtração
+let resultadoSubtracao = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${resultadoSubtracao}`);
+
+// Multiplicação
+let resultadoMultiplicacao = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${resultadoMultiplicacao}`);
+
+// Divisão
+let resultadoDivisao = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${resultadoDivisao}`);
+
+// Módulo (resto da divisão)
+let resultadoModulo = numA % numB;
+console.log(`Resultado do módulo entre ${numA} e ${numB} é ${resultadoModulo}`);
